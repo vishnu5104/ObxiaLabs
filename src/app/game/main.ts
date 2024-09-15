5ec9c777a22a781a18827b0ced7177bca2933547
vishnu5104 <vishnus510412@gmail.com>
2024-09-15 22:38:05 +0530

    game component added for game creation

---
+++ src/app/game/main.ts
@@ -0,0 +1,32 @@
+import { Boot } from "./scenes/Boot";
+import { GameOver } from "./scenes/GameOver";
+import { Game as MainGame } from "./scenes/Game";
+import { MainMenu } from "./scenes/MainMenu";
+import { AUTO, Game } from "phaser";
+import { Preloader } from "./scenes/Preloader";
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+
+const speedDown = 300;
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    width: 1024,
+    height: 768,
+    parent: "game-container",
+    backgroundColor: "#028af8",
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: speedDown },
+            debug: true,
+        },
+    },
+    scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+};
+
+const StartGame = (parent: string) => {
+    return new Game({ ...config, parent });
+};
+
+export default StartGame;
