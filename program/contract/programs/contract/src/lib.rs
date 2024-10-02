6ee52b1005e631fb12d81f05a4bcce9ae6b2ed48
vishnu5104 <vishnus510412@gmail.com>
2024-10-02 22:50:47 +0530

    Program Updated

--- program/contract/programs/contract/src/lib.rs
+++ program/contract/programs/contract/src/lib.rs
@@ -2,12 +2,7 @@ use anchor_lang::prelude::*;
 
 use mpl_core::{instructions::CreateCollectionV2CpiBuilder, ID as MPL_CORE_ID};
 
-use mpl_core::types::{
-    AppDataInitInfo, ExternalPluginAdapterInitInfo, ExternalPluginAdapterSchema, FreezeDelegate,
-    Plugin, PluginAuthority, PluginAuthorityPair,
-};
-
-declare_id!("2Q3mjBxjxmjafN75iv4gbMVfWSSZDKDebebUz5DZrGyv");
+declare_id!("FSCrRyV4Ed1JsuPEA88BBHHob6i9FGktVynL2mfLjx38");
 
 #[derive(AnchorDeserialize, AnchorSerialize)]
 pub struct CreateCollectionArgs {
@@ -28,30 +23,6 @@ pub mod create_core_collection_example {
             None => None,
         };
 
-        let mut plugins: Vec<PluginAuthorityPair> = vec![];
-
-        plugins.push(PluginAuthorityPair {
-            plugin: Plugin::FreezeDelegate(FreezeDelegate { frozen: true }),
-            authority: Some(PluginAuthority::UpdateAuthority),
-        });
-
-        let mut external_plugin_adapters: Vec<ExternalPluginAdapterInitInfo> = vec![];
-
-        let data_authority = ctx
-            .accounts
-            .update_authority
-            .as_ref()
-            .map(|ua| ua.key())
-            .unwrap_or_else(|| ctx.accounts.payer.key());
-
-        external_plugin_adapters.push(ExternalPluginAdapterInitInfo::AppData(AppDataInitInfo {
-            init_plugin_authority: Some(PluginAuthority::UpdateAuthority),
-            data_authority: PluginAuthority::Address {
-                address: data_authority,
-            },
-            schema: Some(ExternalPluginAdapterSchema::Binary),
-        }));
-
         CreateCollectionV2CpiBuilder::new(&ctx.accounts.mpl_core_program.to_account_info())
             .collection(&ctx.accounts.collection.to_account_info())
             .payer(&ctx.accounts.payer.to_account_info())
@@ -59,8 +30,6 @@ pub mod create_core_collection_example {
             .system_program(&ctx.accounts.system_program.to_account_info())
             .name(args.name)
             .uri(args.uri)
-            .plugins(plugins)
-            .external_plugin_adapters(external_plugin_adapters)
             .invoke()?;
 
         Ok(())
