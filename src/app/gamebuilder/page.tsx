6ca6140b5f97c171b74b57be7f5bd4b70208d33f
vishnu5104 <vishnus510412@gmail.com>
2024-10-07 16:13:53 +0530

    game update

--- src/app/gamebuilder/page.tsx
+++ src/app/gamebuilder/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const gamebuilder = () => {
-  return <div>Make your web3 game using your own nfts and get Monetize</div>;
+  return (
+    <div>
+      Make your web3 game using your own nfts and get Monetize your game
+    </div>
+  );
 };
 
 export default gamebuilder;
